69d676094d927d83598f6670a3cebf74cd98c3a5
NavaFee <1457660144@qq.com>
2023-09-29 22:37:59 +0800

    log

--- deploy/04-mint.js
+++ deploy/04-mint.js
@@ -15,11 +15,10 @@ module.exports = async ({ getNamedAccounts }) => {
     const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
     const dynamicSvgNftMintTx = await dynamicSvgNft.mintNft(highValue)
     await dynamicSvgNftMintTx.wait(1)
-    console.log("=====================================================")
+
     console.log(
         `Dynamic SVG NFT index 0 tokenURI: ${await dynamicSvgNft.tokenURI(0)}`
     )
-    console.log("=====================================================")
 
     // Random IPFS NFT
     const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
