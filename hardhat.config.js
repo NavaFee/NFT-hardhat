f1eff49e7c1bcb9c8499f6ede24c94210764a79e
NavaFee <1457660144@qq.com>
2023-10-26 20:47:21 +0800

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,9 +19,9 @@ module.exports = {
     networks: {
         hardhat: {
             chainId: 31337,
-            forking: {
-                url: MAINNET_RPC_URL,
-            },
+            // forking: {
+            //     url: MAINNET_RPC_URL,
+            // },
         },
         sepolia: {
             url: SEPOLIA_RPC_URL,
